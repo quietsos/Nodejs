229ffd5fe20352363eb4a88e214fa8b06e3fec22
quietsos <shohaniu8@gmail.com>
2020-12-16 20:04:26 +0600

    Weather status app

--- weather_app/index.js
+++ weather_app/index.js
@@ -9,11 +9,12 @@ const homefile = fs.readFileSync("home.html","utf-8");
 
 const repalaceVal = (tempval, orgVal) =>{
     let temperature = tempval.replace("{%tempval%}", orgVal.main.temp);
-    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp);
-    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp);
-    temperature = temperature.replace("{%location%}", orgVal.main.temp);
-    temperature = temperature.replace("{%country%}", orgVal.main.temp);
-   
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main)
+    return temperature;
 
 };
 
@@ -29,10 +30,19 @@ const server = http.createServer((req,res) =>{
                 // console.log(objdata);
                 // console.log(arrData);
                 // console.log(arrData[0].main.temp);
-                const reatTimeData =arrData.map((val) =>{
-                    console.log(val.main);
-                    // repalaceVal(homefile,val);
-                });
+                // const realTimeData =arrData.map((val) =>{
+                //     console.log(val.main);
+                //     console.log(val.timezone);
+                //     console.log(val.wind);
+                //     console.log(val.sys);
+                //     repalaceVal(homefile,val);
+                //     // res.write(realTimeData);
+                //     console.log(realTimeData);
+                // });
+
+                const realTimeData = arrData.map((val) => repalaceVal(homefile,val)).join("");
+                console.log(realTimeData);
+                res.write(realTimeData);
 
               })
 
@@ -40,6 +50,7 @@ const server = http.createServer((req,res) =>{
             .on("end", (err) => {
                 if(err) return console.log("Connection closed due to errors", err);
                 console.log("end");
+                res.end();
               });
         }
 });
